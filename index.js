1349521299d972c4b156b82b1245b9bf97d9ba30
Алексей Савельев <mtptz21@gmail.com>
2023-03-08 10:57:22 +0300

    feat: add new task

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(123);
+function centuryFromYear(year) {
+  let centuryCount = 0;
+  while (year > 0) {
+    year = year - 100;
+    centuryCount = centuryCount + 1;
+  }
+  return centuryCount;
+}
+centuryFromYear();
